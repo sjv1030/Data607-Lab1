95b2d927c508d5c9d5eb9d0fafd5797aaa532636
sjv1030 <31420197+sjv1030@users.noreply.github.com>
2017-09-02 21:26:44 -0400

    Add files via upload

---
+++ lab1_answer.R
@@ -0,0 +1,35 @@
+
+require(plyr)
+require(curl)
+
+df <-  read.table(curl("https://raw.githubusercontent.com/sjv1030/Data607-Lab1/master/agaricus-lepiota.data"),sep=",")
+dim(df)
+
+df_e <- subset(df[1],V1 == "e")
+nrow(df_e)
+
+df_p <- subset(df[1],V1 == "p")
+nrow(df_p)
+
+ans <- df[c(1,21,23,4)]
+colnames(ans) <- c("e_p","spore_print_color","habitat","cap_color")
+
+ans$e_p <- mapvalues(ans$e_p, from=c("e","p"), to=c("edible","poisonous"))
+
+ans$spore_print_color <- mapvalues(ans$spore_print_color, 
+                                   from=c("k","n","b","h","r","o","u","w","y"),
+                                   to=c("black","brown","buff","chocolate","green",
+                                        "orange","purple","white","yellow"))
+
+ans$habitat <- mapvalues(ans$habitat, from=c("g","l","m","p","u","w","d"), 
+                         to=c("grasses","leaves","meadows","path","urban","waste","woods"))
+
+ans$cap_color <- mapvalues(ans$cap_color, 
+                           from=c("n","b","r","u","w","y","c","g","p","e"),
+                           to=c("brown","buff","green","purple","white",
+                                "yellow","cinnamon", "gray","pink","red"))
+
+
+# picked habitat and cap-color because together they have 100% accuracy
+# when former = leaves and latter = white
+# picked spore-print-color because it has a 99.41% accuracy when value is green
